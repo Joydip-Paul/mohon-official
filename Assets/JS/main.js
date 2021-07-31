4ac99abf6e444c3b6ee23314435445f81578de15
Joydip Paul <joydippaul72@gmail.com>
2021-08-01 03:19:58 +0600

    upload diye daw bro.

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -2,3 +2,19 @@ function toggle() {
   var header = document.getElementById("header");
   header.classList.toggle("active");
 }
+
+
+// Scroll Button
+var myButton = document.getElementById("myBtn");
+window.onscroll = function() {Jishu()};
+function Jishu() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
+  }
+}
+function Joydip() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
